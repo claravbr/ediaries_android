563b892cc8262966bdeceb6f6e582287901ce35f
claravbr <clara.vbr@gmail.com>
2023-06-22 21:11:17 +0200

    Layout formularios

---
+++ app/src/main/java/com/uclm/louise/ediaries/ExpLVAdapter.java
@@ -0,0 +1,72 @@
+package com.uclm.louise.ediaries;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ExpLVAdapter extends BaseExpandableListAdapter {
+
+    private ArrayList<String> listPregunta;
+    private Map<String, String> mapChild;
+    private Context context;
+
+    public ExpLVAdapter(ArrayList<String> listPregunta, Map<String, String> mapChild, Context context) {
+        this.listPregunta = listPregunta;
+        this.mapChild = mapChild;
+        this.context = context;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
