cdef2e2b2dd3d8777b82d576f470c1f12d81ad42
claravbr <clara.vbr@gmail.com>
2023-07-04 16:33:17 +0200

    Revert "ED-26 Creación de las actividades de los layouts"
    This reverts commit d973d7536b90c8c66d9b14e69c9262d176c89074.

--- app/src/main/java/com/uclm/louise/ediaries/MainActivity.java
+++ app/src/main/java/com/uclm/louise/ediaries/MainActivity.java
@@ -1,63 +1,45 @@
 package com.uclm.louise.ediaries;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonMisTareas;
-    private Button buttonMiProgreso;
-    private Button buttonConsejos;
-    private Button buttonComoEstoy;
+    private ExpandableListView listaconsejos;
+    private ExpLVAdapter adapter;
+    private ArrayList<String> listPregunta;
+    private Map<String, String> mapChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_principal);
+        setContentView(R.layout.consejos);
 
-        Toolbar toolbar = findViewById(R.id.topAppBar);
-        setSupportActionBar(toolbar);
+        listaconsejos = (ExpandableListView) findViewById(R.id.listaconsejos);
+        listPregunta = new ArrayList<>();
+        mapChild = new HashMap<>();
 
-        buttonMisTareas = findViewById(R.id.option1Button);
-        buttonMisTareas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, TareaspendientesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        buttonMiProgreso = findViewById(R.id.option2Button);
-        buttonMiProgreso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MiProgresoActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        buttonConsejos = findViewById(R.id.option3Button);
-        buttonConsejos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Consejos.class);
-                startActivity(intent);
-            }
-        });
-
-        buttonComoEstoy = findViewById(R.id.option4Button);
-        buttonComoEstoy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, DiarioEmocionesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        }
+        cargarDatos();
     }
+
+    private void cargarDatos(){
+        listPregunta.add("¿Cómo mejoro mi atención?");
+        listPregunta.add("¿Cómo mejoro mi memoria?");
+        listPregunta.add("¿Qué puedo hacer para relajarme?");
+        listPregunta.add("¿Cómo puedo resolver un problema?");
+
+        mapChild.put(listPregunta.get(0), "Establece un entorno de estudio tranquilo y libre de distracciones.");
+        mapChild.put(listPregunta.get(1), "Utiliza técnicas de estudio que te ayuden a retener información, como resumir o hacer esquemas.");
+        mapChild.put(listPregunta.get(2), "Aprende técnicas de respiración profunda y relajación muscular para reducir el estrés y la ansiedad.");
+        mapChild.put(listPregunta.get(3), "Divide el problema en pasos más pequeños y abórdalos de forma secuencial.");
+
+        adapter = new ExpLVAdapter(listPregunta, mapChild, this);
+        listaconsejos.setAdapter((adapter));
+    }
+}
