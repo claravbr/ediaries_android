ca7885bcc6e335fa7d4851406c33d33c3e715a5c
claravbr <clara.vbr@gmail.com>
2023-10-30 18:55:19 +0100

    ED-19 Muestra la fecha actual

--- app/src/main/java/com/uclm/louise/ediaries/activity/DiarioEmocionesActivity.java
+++ app/src/main/java/com/uclm/louise/ediaries/activity/DiarioEmocionesActivity.java
@@ -7,9 +7,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.uclm.louise.ediaries.R;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Date;
+import java.util.Locale;
 
 public class DiarioEmocionesActivity extends AppCompatActivity {
 
@@ -20,6 +27,10 @@ public class DiarioEmocionesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.diario_emociones);
+
+        setDate();
+
+
 
         buttonSave = findViewById(R.id.buttonSave);
 
@@ -34,5 +45,14 @@ public class DiarioEmocionesActivity extends AppCompatActivity {
                 Log.i("Info log", "Vuelta al menu principal");
             }
         });
+    }
+
+    private void setDate() {
+        Month mes = LocalDate.now().getMonth();
+        String nombreMes = mes.getDisplayName(TextStyle.FULL, new Locale("es", "ES"));
+
+        TextView textViewDate = findViewById(R.id.textViewDate);
+
+        textViewDate.setText("Fecha: " + LocalDate.now().getDayOfMonth() + " de " + nombreMes + " de " + LocalDate.now().getYear());
     }
 }
